a0854f9c691a2f330dd6f92a7b4787b4084e7510
niksasi <niksasi@gmail.com>
2015-02-20 20:57:34 -0500

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Course3_Project
-## hey this is a test, weee.
+## Course Project: Getting and Cleaning Data
 
+Data from  Reyes-Ortiz et al. entitled "Human Activity Recognition Using Smartphones Dataset"  was downloaded from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip. The file UCI HAR Dataset 2 was unzipped into my working directory. This dataset contains smartphone recordings from 30 subjects, each of whom conducted 6 different movement tasks (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING). Linear acceleration and angular velocity in multiple dimensions were recorded while performing tasks and a side set of different measurements were calculated. Two datasets were provided one from a training group of subjects and another from the testing group of subjects. 
+
+For this Course Project, Training and Test dataset were merged together and properly annotated with variable names, subject names and activity labels. This assembled dataframe was then subsetted to isolate only variables/columns that contained the words "mean" or "std" in the variable name. The subsetted dataframe was then grouped by Subject and Activity. The script `run_analysis.R` was written to complete all of these tasks and to output a dataframe (df3), which displays the tidy dataframe. The script is divided into two parts: Part 1 reads in all relevant .txt files from within UCI HAR Dataset 2 and Part 2 assembles it together to make a dataframe (Final_RAW_2) and then cleans it up to generate a tidy dataframe (df3). The output of the script opens up the Viewer in R to look at the tidy dataframe df3. 
+
+####Part 1: Reading in all files from the UCI HAR Dataset 2 folder:
+
+- Each relevant .txt file was read into R and assigned a variable. Names of variables were chosen to match the names of the .txt files. Data included the training data set, the test data set and the respective subject, activity labels and headers (variables) of the training and test data. 
+
+####Part 2: Assembling all the parts of the Samsung data together and generating tidy data:
+
+- This part of the code starts by putting together the original data files like legos using a series of rbind and cbind operations    
+
+- A first version of the entire dataset is created and defined as Final_RAW_2    
+
+- Then the headers were assigned to FINAL_RAW_2 using the variable names provided. Two additional names were added for "Subject" and "Activity"  
+
+- Weird thing, on my computer R gets stuck at this point, if I dont do this step to redefine FINAL_RAW_2 as a new dataframe FINAL. Not sure why?  
+
+- Then FINAL is subsetted using contains () to select only the columns which contain "mean" or "std" in their header and assigned to DFmeanSD.  
+
+- The Activity column of DFmeanSD is numeric. This is changed to a factor with labels. The provided file activity_labels.txt is used to define factor labels  
+
+- Then the dataframe DF_MeanSD is grouped by Subject and then Activity and assigned to df2  
+
+- Means are then calculated using summarise_each for all columns for df2 and assigned to df3.  
+
+- df3 is the final tidy data set  
+
+- View is used for easy visualization of df3  
--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,30 @@
 ##Part 1: Reading in all files from the UCI HAR Dataset 2 folder:
 
-features <- read.table("./UCI HAR Dataset 2/features.txt", stringsAsFactors = FALSE)
-X_train <- read.table("./UCI HAR Dataset 2/train/X_train.txt")
-subject_train<- read.table("./UCI HAR Dataset 2/train/subject_train.txt")
-y_train <- read.table("./UCI HAR Dataset 2/train/y_train.txt")
-X_test <- read.table("./UCI HAR Dataset 2/test/X_test.txt")
-subject_test <- read.table("./UCI HAR Dataset 2/test/subject_test.txt")
-y_test <- read.table("./UCI HAR Dataset 2/test/y_test.txt")
-activitylabels <- read.table("./UCI HAR Dataset 2/activity_labels.txt", stringsAsFactors = FALSE)
+## Please note that I have annotated what each line of code does by documenting with a hash tag after the code.
+
+features <- read.table("./UCI HAR Dataset 2/features.txt", stringsAsFactors = FALSE) #Contains all headers and variable names of the complete data set
+X_train <- read.table("./UCI HAR Dataset 2/train/X_train.txt") # Contains the training data set
+subject_train<- read.table("./UCI HAR Dataset 2/train/subject_train.txt") # Contains the subject numbers for X_train
+y_train <- read.table("./UCI HAR Dataset 2/train/y_train.txt") # Contains the numerical ID of the activity labels for X_train
+X_test <- read.table("./UCI HAR Dataset 2/test/X_test.txt") # Contains the test data set
+subject_test <- read.table("./UCI HAR Dataset 2/test/subject_test.txt") # Contains the subject numbers for X_test
+y_test <- read.table("./UCI HAR Dataset 2/test/y_test.txt") # Contains the numerical ID of the activity labels for X_test
+activitylabels <- read.table("./UCI HAR Dataset 2/activity_labels.txt", stringsAsFactors = FALSE) #Contains a key with the character names of the activity labels used in the training and test data sets
 
 ## Part 2: Assembling all the parts of the Samsung data together and generating tidy data:
 
-Final_RAW_1 <- rbind(X_train, X_test) 
-Full_subject <- rbind(subject_train, subject_test)
-Full_y <- rbind(y_train, y_test)
-Final_RAW_2 <- cbind(Full_y, Full_subject, Final_RAW_1) 
-names(Final_RAW_2) <- c("Activity", "Subject", features[,2])
-FINAL <- Final_RAW_2[1:10299,1:563] ## weird thing, on my computer R gets stuck, if I dont do this step to redefine the df
-DF_MeanSD <- cbind(FINAL[,1:2],select(FINAL, contains("mean")),select(FINAL, contains("std")))
-DF_MeanSD[,1] <- factor(DF_MeanSD[,1], labels = activitylabels[,2])
-df2 <- group_by(DF_MeanSD, Subject, Activity)
-df3 <- summarise_each(df2, funs(mean))
+## Please note that I have annotated what each line of code does by documenting with a hash tag after the code.
+
+Final_RAW_1 <- rbind(X_train, X_test)  #Joins the training and the test data set, 1 below the other
+Full_subject <- rbind(subject_train, subject_test) # Joins the subject numbers for both training and test data sets
+Full_y <- rbind(y_train, y_test) # Joins the numeric activity labels for both training and test data sets
+Final_RAW_2 <- cbind(Full_y, Full_subject, Final_RAW_1) # Makes a complete data set of all training and test data, along with subject and activity labels
+names(Final_RAW_2) <- c("Activity", "Subject", features[,2]) # Adds the headers to the complete dataset using features.txt and manually including headers for Subject and Activity
+names(Final_RAW_2) <- make.names(names(Final_RAW_2)) # Converts all variable names to an R compatible format
+FINAL <- Final_RAW_2[1:10299,1:563] # weird thing, on my computer R gets stuck, if I dont do this step to redefine the df
+DF_MeanSD <- cbind(FINAL[,1:2],select(FINAL, contains("mean")),select(FINAL, contains("std"))) # Generates a subset containing only columns that have "mean" or "std" in the headers
+DF_MeanSD[,1] <- factor(DF_MeanSD[,1], labels = activitylabels[,2]) # converts the Activity column from numeric to Factor class and then assigns the labels using activity_labels.txt
+df2 <- group_by(DF_MeanSD, Subject, Activity) # Groups the dataframe by Subject and Activity
+df3 <- summarise_each(df2, funs(mean)) # Find the means of the remaining columns while grouped with Subject
 View(df3)
 
